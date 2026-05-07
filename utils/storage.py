3725359b061700fa89c8c79c9bc125e455f3f644
Aengus MacKenzie <aengus.2000@gmail.com>
2026-05-07 14:51:49 -0400

    added full user JSON persistence

--- utils/storage.py
+++ utils/storage.py
@@ -1,5 +1,7 @@
 import json
 
+from models.user import User
+
 DATA_FILE = "data/tracker_data.json"
 
 
@@ -13,3 +15,24 @@ def load_data(file_path=DATA_FILE):
 def save_data(data, file_path=DATA_FILE):
     with open(file_path, "w") as file:
         json.dump(data, file, indent=2)
+
+def save_users(users, file_path=DATA_FILE):
+    data = {
+        "users": [user.to_dict() for user in users],
+        "projects": [],
+        "tasks": []
+    }
+
+    save_data(data, file_path)
+
+
+def load_users(file_path=DATA_FILE):
+    data = load_data(file_path)
+
+    users = []
+
+    for user_data in data["users"]:
+        user = User.from_dict(user_data)
+        users.append(user)
+
+    return users
